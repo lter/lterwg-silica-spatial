0110979017ee502d20fdae63d220399101b590e1
njlyon0 <nicholasjlyon@gmail.com>
2022-03-28 15:13:15 -0700

    Added comparison of extraction (raster-raster) within two different coordinate reference systems (CRS) to see if results differ dramatically

--- extract-method-diagnostic.R
+++ extract-method-diagnostic.R
@@ -5,9 +5,8 @@
 ## Nick J Lyon + 
 
 # Purpose:
-## Compare different methods of extraction of landcover data to ensure consistent outcomes regardless of package(s)/function(s) used
-
-# Housekeeping ---------------------------------------------------------------
+## 1 - Compare different methods of extraction of landcover data to ensure consistent outcomes regardless of package(s)/function(s) used
+## 2 - Compare different coordinate reference systems (CRS) across the same extraction method to see if we should be re-projecting from WGS84 to something that accounts for changing degree:area relationship at high latitudes
 
 # Read needed libraries
 library(tidyverse); library(sf); library(stars); library(terra); library(exactextractr)
@@ -23,7 +22,7 @@ setwd(path)
 ## Check that it worked
 getwd()
 
-# Housekeeping -------------------------------------------------------------
+# Extraction Housekeeping -----------------------------------------------------
 
 # Grab the shapefiles for all of the watersheds
 sheds_all <- sf::st_read('watershed-shapefiles/SilicaSynthesis_allWatersheds.shp')
@@ -116,5 +115,102 @@ data_all <- data_df_ext %>%
 
 # Check contents
 data_all
+
+# Clean up environment
+rm(list = setdiff(ls(), c('path', 'sheds_sf')))
+
+# CRS Housekeeping -----------------------------------------------------------
+
+# Prepare two CRS variants of the sf polygon
+sheds_sf_4326 <- sheds_sf
+sheds_sf_5070 <- sheds_sf %>%
+  st_transform(crs = 5070)
+
+# Check them both
+st_crs(sheds_sf_4326)
+st_crs(sheds_sf_5070)
+
+# Make them both rasters
+sheds_rast_4326 <- terra::vect(sheds_sf_4326)
+sheds_rast_5070 <- terra::vect(sheds_sf_5070)
+
+# Check that they transferred correctly
+crs(sheds_rast_4326)
+crs(sheds_rast_5070)
+
+# Read in the lithology data raster
+data_rast_4326 <- terra::rast("extracted-data/raw-lithology-data/glim_wgs84_0point5deg.txt.asc")
+
+# Check CRS
+crs(data_rast_4326)
+
+# Re-project into a different CRS to check it out
+## NOTE: will do this once but save the product, then comment out the workflow and just read in the different CRS version
+# data_rast_5070 <- terra::project(data_rast_4326, y = 'epsg:5070')
+# terra::writeRaster(data_rast_5070, "extracted-data/raw-lithology-data/lithology_epsg5070_raster.tiff")
+
+# Read in the re-projected file and check the CRS
+data_rast_5070 <- terra::rast("extracted-data/raw-lithology-data/lithology_epsg5070_raster.tiff")
+crs(data_rast_5070)
+
+# Make an exploratory plot
+# (`if` bit just to make the whole block run together)
+if(1 == 1){
+  par(mfrow = c(2, 1))
+  ## First data raster with sf overlay
+  plot(data_rast_5070, axes = T, reset = F, main = 'EPSG 5070')
+  plot(sheds_rast_5070, axes = T, add = T)
+  ## Then data raster with raster overlay
+  plot(data_rast_4326, axes = T, reset = F, main = 'EPSG 4326')
+  plot(sheds_rast_4326, axes = T, add = T)
+  par(mfrow = c(1, 1))}
+
+# Extract Data from both CRS Variants --------------------------------------
+
+# Extract from EPSG 4326 first
+data_df_4326 <- data_rast_4326 %>%
+  terra::extract(y = sheds_rast_4326) %>%
+  # Get column names to match
+  rename(uniqueID = ID, value = glim_wgs84_0point5deg.txt) %>%
+  # Re-create the uniqueID column
+  mutate(uniqueID = ifelse(uniqueID == 1, yes = "GRO_Yukon", no = "GRO_Yukon")) %>%
+  # Count instances of each pixel category
+  group_by(uniqueID, value) %>%
+  summarise(num_pixels_4326 = n())
+
+# Look at it
+data_df_4326
+
+# Now extract from EPSG 5070
+data_df_5070 <- data_rast_5070 %>%
+  terra::extract(y = sheds_rast_5070) %>%
+  # Get column names to match
+  rename(uniqueID = ID) %>%
+  # The conversion subtly changed the integer codes for land areas so fix them
+  mutate(value = as.integer(gsub("\\.", "", str_sub(glim_wgs84_0point5deg.txt, 1, 2)))) %>%
+  # Pare down to only needed columns
+  select(uniqueID, value) %>%
+  # Re-create the uniqueID column
+  mutate(uniqueID = ifelse(uniqueID == 1, yes = "GRO_Yukon", no = "GRO_Yukon")) %>%
+  # Count instances of each pixel category
+  group_by(uniqueID, value) %>%
+  summarise(num_pixels_5070 = n())
+
+# Look at this one
+data_df_5070
+
+# Combine Pixel Counts for Comparison --------------------------------------
+
+# Make them the same object to make it easier to compare
+data_both_crs <- data_df_4326 %>%
+  full_join(data_df_5070, by = c("uniqueID", "value")) %>%
+  # Identify differences
+  mutate(`4326_vs_5070_diff` = num_pixels_4326 - num_pixels_5070)
+
+# Check contents
+data_both_crs
+
+# Clean up environment
+rm(list = setdiff(ls(), c('path', 'sheds_sf')))
 
 # End ----------------------------------------------------------------------
