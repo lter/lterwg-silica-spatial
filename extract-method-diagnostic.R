44ab740dd96c78cc7602f688f1c542836fa72326
njlyon0 <nicholasjlyon@gmail.com>
2022-03-29 15:18:53 -0700

    Changed re-projection to use nearest neighbor to avoid altering integer codes for rock types. Also compared percent differences between CRS rather than absolute pixel count to see how much CRS affects outcome (not very much it looks like!)

--- extract-method-diagnostic.R
+++ extract-method-diagnostic.R
@@ -22,8 +22,6 @@ setwd(path)
 ## Check that it worked
 getwd()
 
-# Extraction Housekeeping -----------------------------------------------------
-
 # Grab the shapefiles for all of the watersheds
 sheds_all <- sf::st_read('watershed-shapefiles/SilicaSynthesis_allWatersheds.shp')
 
@@ -34,6 +32,8 @@ sheds_sf <- sheds_all %>%
 
 # Check that out
 str(sheds_sf)
+
+# Extraction Housekeeping -----------------------------------------------------
 
 # Get a raster version
 sheds_rast <- terra::vect(sheds_sf)
@@ -122,7 +122,8 @@ rm(list = setdiff(ls(), c('path', 'sheds_sf')))
 # CRS Housekeeping -----------------------------------------------------------
 
 # Prepare two CRS variants of the sf polygon
-sheds_sf_4326 <- sheds_sf
+sheds_sf_4326 <- sheds_sf %>%
+  st_transform(crs = 4326)
 sheds_sf_5070 <- sheds_sf %>%
   st_transform(crs = 5070)
 
@@ -139,19 +140,25 @@ crs(sheds_rast_4326)
 crs(sheds_rast_5070)
 
 # Read in the lithology data raster
-data_rast_4326 <- terra::rast("extracted-data/raw-lithology-data/glim_wgs84_0point5deg.txt.asc")
+data_rast_raw <- terra::rast("extracted-data/raw-lithology-data/glim_wgs84_0point5deg.txt.asc")
 
 # Check CRS
-crs(data_rast_4326)
+crs(data_rast_raw)
 
-# Re-project into a different CRS to check it out
+# Re-project into the different CRS's to check it out
 ## NOTE: will do this once but save the product, then comment out the workflow and just read in the different CRS version
-# data_rast_5070 <- terra::project(data_rast_4326, y = 'epsg:5070')
-# terra::writeRaster(data_rast_5070, "extracted-data/raw-lithology-data/lithology_epsg5070_raster.tiff")
+## 5070
+# data_rast_5070 <- terra::project(data_rast_raw, y = 'epsg:5070', method = 'near')
+# terra::writeRaster(data_rast_5070, "extracted-data/raw-lithology-data/lithology_epsg5070_raster.tiff", overwrite = T)
+## 4326
+# data_rast_4326 <- terra::project(data_rast_raw, y = 'epsg:4326', method = "near")
+# terra::writeRaster(data_rast_4326, "extracted-data/raw-lithology-data/lithology_epsg4326_raster.tiff", overwrite = T)
 
 # Read in the re-projected file and check the CRS
 data_rast_5070 <- terra::rast("extracted-data/raw-lithology-data/lithology_epsg5070_raster.tiff")
 crs(data_rast_5070)
+data_rast_4326 <- terra::rast("extracted-data/raw-lithology-data/lithology_epsg4326_raster.tiff")
+crs(data_rast_4326)
 
 # Make an exploratory plot
 # (`if` bit just to make the whole block run together)
@@ -167,6 +174,14 @@ if(1 == 1){
 
 # Extract Data from both CRS Variants --------------------------------------
 
+# Double check that the CRS match
+crs(data_rast_4326) == crs(sheds_rast_4326)
+## They don't....
+
+# Check what they are
+crs(data_rast_4326)
+crs(sheds_rast_4326)
+
 # Extract from EPSG 4326 first
 data_df_4326 <- data_rast_4326 %>%
   terra::extract(y = sheds_rast_4326) %>%
@@ -181,15 +196,14 @@ data_df_4326 <- data_rast_4326 %>%
 # Look at it
 data_df_4326
 
+# Check the other CRS
+crs(data_rast_5070) == crs(sheds_rast_5070)
+
 # Now extract from EPSG 5070
 data_df_5070 <- data_rast_5070 %>%
   terra::extract(y = sheds_rast_5070) %>%
   # Get column names to match
-  rename(uniqueID = ID) %>%
-  # The conversion subtly changed the integer codes for land areas so fix them
-  mutate(value = as.integer(gsub("\\.", "", str_sub(glim_wgs84_0point5deg.txt, 1, 2)))) %>%
-  # Pare down to only needed columns
-  select(uniqueID, value) %>%
+  rename(uniqueID = ID, value = glim_wgs84_0point5deg.txt) %>%
   # Re-create the uniqueID column
   mutate(uniqueID = ifelse(uniqueID == 1, yes = "GRO_Yukon", no = "GRO_Yukon")) %>%
   # Count instances of each pixel category
@@ -205,10 +219,30 @@ data_df_5070
 data_both_crs <- data_df_4326 %>%
   full_join(data_df_5070, by = c("uniqueID", "value")) %>%
   # Identify differences
-  mutate(`4326_vs_5070_diff` = num_pixels_4326 - num_pixels_5070)
+  mutate(num_diff = num_pixels_4326 - num_pixels_5070)
+
+# We want percents
+data_both_crs_perc <- data_both_crs %>%
+  # Pivot to long format
+  pivot_longer(cols = starts_with('num_pixels'),
+               names_to = 'source', values_to = 'pixel_ct') %>%
+  # Group and compute percent differences
+  group_by(uniqueID, source) %>%
+  mutate(total_pixels = sum(pixel_ct, na.rm = T)) %>%
+  ungroup() %>%
+  group_by(uniqueID, source, num_diff, value) %>%
+  mutate(pixel_perc = (pixel_ct / total_pixels) * 100 ) %>%
+  # Remove count columns
+  select(-pixel_ct, -total_pixels) %>%
+  # Edit the source column
+  mutate(source = gsub("num_", "perc_", source)) %>%
+  # Pivot to wide format
+  pivot_wider(names_from = source, values_from = pixel_perc) %>%
+  # Calculate percent difference
+  mutate(perc_diff = round(perc_pixels_4326 - perc_pixels_5070, digits = 2))
 
 # Check contents
-data_both_crs
+data_both_crs_perc
 
 # Clean up environment
 rm(list = setdiff(ls(), c('path', 'sheds_sf')))
