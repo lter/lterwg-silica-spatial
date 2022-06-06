d398443472846a65a5ce162a4fcc9a9123164c8a
njlyon0 <nicholasjlyon@gmail.com>
2022-06-06 07:28:16 -0700

    Added comments and integrated Julien's merged branch more seamlessly

--- filepath-demo.R
+++ filepath-demo.R
@@ -2,22 +2,24 @@
         # Universal File Path Demonstration
 ## ----------------------------------------------- ##
 
+## Option 1 : Using Paths ----
+# You can set the paths (to your home folder and the shares folder) as objects and just add them into the functions as needed
 
+# Set path to shares folder as an object
+base_path <- file.path("/", "home", "shares", "lter-si", "gdrive")
 
-## Option 1 : Using paths
+# Set the path within your folder of interest to the data folder we want to look in as a different object
+basin_path <- file.path("Master_Dataset", "BasinCharacteristics")
 
-# Set path and filenames
-base_path <- "/home/shares/lter-si/gdrive"
-basin_path <- "Master_Dataset/BasinCharacteristics"
-
+# And identify the file as an object
 watershed_file <- "LongTermWatersheds_LatLong_drainArea.csv"
 
-# read the file
+# Read the file
 test_data <- read.csv(file = file.path(base_path, basin_path, watershed_file))
+str(test_data)
 
-
-
-## Option 2 : You can also change the working directory (if you need to read a lot of files)
+## Option 2 : Changing the Working Directory ----
+# You can also change the working directory (if you need to read/write a lot of files)
 
 # Default working directory is the folder containing the current project
 getwd()
@@ -37,7 +39,7 @@ shares <- getwd()
 shares
 
 # Now you can read in (or save out) files directly from the "shares" folder
-test_data <- read.csv(file = "LongTermWatersheds_LatLong_drainArea.csv")
+test_data <- read.csv(file = watershed_file)
 str(test_data)
 
 # If you scan through the above you'll see that this script could be run by anyone with access to your group's "shares" sub-folder ("lter-si" in this case)
