e678c2a38c54ea6e755d83a1a1380847f3a069d2
angelchen <angelchen7777@yahoo.com>
2023-01-27 15:27:50 -0800

    started npp extraction script

---
+++ extract-npp.R
@@ -0,0 +1,193 @@
+## ------------------------------------------------------- ##
+#       Silica WG - Extract Spatial Data - Greenup
+## ------------------------------------------------------- ##
+# Written by:
+## Angel Chen
+
+# Purpose:
+## Using the watershed shapefiles created in "id-watershed-polygons.R"
+## Extract the following data: NPP
+
+## ------------------------------------------------------- ##
+#                    Housekeeping ----
+## ------------------------------------------------------- ##
+
+# Read needed libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse, sf, ncdf4, stars, terra, exactextractr, 
+                 NCEAS/scicomptools, googledrive)
+
+# Clear environment
+rm(list = ls())
+
+# Identify path to location of shared data
+(path <- scicomptools::wd_loc(local = F, remote_path = file.path('/', "home", "shares", "lter-si", "si-watershed-extract")))
+
+# Load in site names with lat/longs
+sites <- read.csv(file = file.path(path, "site-coordinates", 'silica-coords_ACTUAL.csv'))
+
+# Check it out
+dplyr::glimpse(sites)
+
+# Grab the shapefiles the previous script (see PURPOSE section) created
+sheds <- sf::st_read(dsn = file.path(path, "site-coordinates", "silica-watersheds.shp"))
+
+# Check that out
+dplyr::glimpse(sheds)
+
+# Clean up environment
+rm(list = setdiff(ls(), c('path', 'sites', 'sheds')))
+
+## ------------------------------------------------------- ##
+#          MCD12Q2 (v. 061) - Identify Files ----
+## ------------------------------------------------------- ##
+
+# Make an empty list
+file_list <- list()
+
+# Identify files for each region
+for(region in c("north-america-usa", "north-america-arctic",
+                "cropped-russia-west", "cropped-russia-west-2",
+                "cropped-russia-center", "cropped-russia-east",
+                "puerto-rico", "scandinavia")){
+  
+  # Identify files in that folder
+  file_df <- data.frame("region" = region,
+                        "files" = dir(path = file.path(path, "raw-driver-data", 
+                                                       "raw-npp", region),
+                                      pattern = "MOD17A3HGF.006_Npp_"))
+  
+  # Add that set of files to the list
+  file_list[[region]] <- file_df }
+
+# Wrangle the list
+file_all <- file_list %>%
+  # Unlist the loop's output
+  purrr::map_dfr(.f = dplyr::select, dplyr::everything()) %>%
+  # Identify date from file name
+  dplyr::mutate(date_raw = stringr::str_extract(string = files, 
+                                                pattern = "_doy[[:digit:]]{7}")) %>%
+  # Simplify that column
+  dplyr::mutate(date = gsub(pattern = "_doy", replacement = "", x = date_raw)) %>%
+  # Identify year
+  dplyr::mutate(year = stringr::str_sub(string = date, start = 1, end = 4)) %>%
+  # Drop 'raw' version
+  dplyr::select(-date_raw) 
+
+# Glimpse it
+dplyr::glimpse(file_all)
+
+# Clean up environment
+rm(list = setdiff(ls(), c('path', 'sites', 'sheds', 'file_all')))
+
+## ------------------------------------------------------- ##
+#     .       Greenup - Bounding Box Check ----
+## ------------------------------------------------------- ##
+# Let's check to make sure each of my manual bounding boxes fits the sites for that region
+
+# Filter to only one row per 'region'
+(viz_files <- file_all %>% 
+   # Find first file per region
+   dplyr::group_by(region) %>%
+   dplyr::summarize(files = dplyr::first(x = files)) %>%
+   dplyr::ungroup() )
+
+# Read in one raster of each region
+rast1 <- terra::rast(file.path(path, "raw-driver-data",  "raw-npp",
+                               viz_files$region[1], viz_files$files[1]))
+rast2 <- terra::rast(file.path(path, "raw-driver-data",  "raw-npp",
+                               viz_files$region[2], viz_files$files[2]))
+rast3 <- terra::rast(file.path(path, "raw-driver-data",  "raw-npp",
+                               viz_files$region[3], viz_files$files[3]))
+rast4 <- terra::rast(file.path(path, "raw-driver-data",  "raw-npp",
+                               viz_files$region[4], viz_files$files[4]))
+rast5 <- terra::rast(file.path(path, "raw-driver-data",  "raw-npp",
+                               viz_files$region[5], viz_files$files[5]))
+rast6 <- terra::rast(file.path(path, "raw-driver-data",  "raw-npp",
+                               viz_files$region[6], viz_files$files[6]))
+rast7 <- terra::rast(file.path(path, "raw-driver-data",  "raw-npp",
+                               viz_files$region[7], viz_files$files[7]))
+rast8 <- terra::rast(file.path(path, "raw-driver-data",  "raw-npp",
+                               viz_files$region[8], viz_files$files[8]))
+
+# Plot each "tile" of data against the watersheds polygons
+## Russia Composite (Cropped)
+frame_rast <- terra::rast(terra::ext(55, 140, 45, 80))
+suppressWarnings(plot(frame_rast, axes = T, reset = F, main = "Russia COMPOSITE"))
+plot(rast1, axes = T, add = T)
+plot(rast3, axes = T, add = T)
+plot(rast4, axes = T, add = T)
+plot(sheds, axes = T, add = T)
+
+## Russia East (Cropped)
+plot(rast2, axes = T, reset = F, main = viz_files$region[2])
+plot(sheds, axes = T, add = T)
+
+## North America Arctic
+plot(rast5, axes = T, reset = F, main = viz_files$region[5])
+plot(sheds, axes = T, add = T)
+
+## USA
+plot(rast6, axes = T, reset = F, main = viz_files$region[6])
+plot(sheds, axes = T, add = T)
+
+## Puerto Rico
+plot(rast7, axes = T, reset = F, main = viz_files$region[7])
+plot(sheds, axes = T, add = T)
+
+## Scandinavia
+plot(rast8, axes = T, reset = F, main = viz_files$region[8])
+plot(sheds, axes = T, add = T)
+
+# Clean up environment
+rm(list = setdiff(ls(), c('path', 'sites', 'sheds', 'file_all')))
+
+## ------------------------------------------------------- ##
+#                    NPP - Extract ----
+## ------------------------------------------------------- ##
+
+for (a_year in "2001"){
+  # Subset to one year
+  one_year_data <- dplyr::filter(file_all, year == a_year)
+  
+  # Make a list to house extracted information for a year
+  year_list <- list()
+  
+  for (i in 1:nrow(one_year_data)){
+    # Read in the raster
+    gr_raster <- terra::rast(file.path(path, "raw-driver-data", "raw-npp", one_year_data$region[i], one_year_data$files[i]))
+    
+    # Extract all possible information from that dataframe
+    ex_data <- exactextractr::exact_extract(x = gr_raster, y = sheds, 
+                                            include_cols = c("river_id"),
+                                            progress = FALSE) %>%
+      # Unlist to dataframe
+      purrr::map_dfr(dplyr::select, dplyr::everything()) %>%
+      # Drop coverage fraction column
+      dplyr::select(-coverage_fraction) %>%         
+      # Make new relevant columns
+      dplyr::mutate(year = a_year,
+                    .after = river_id)
+    
+    # Add this dataframe to the list we made 
+    year_list[[i]] <- ex_data
+    
+    # End message
+    message("Finished extracting raster ", i, " of ", nrow(one_year_data)) 
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
