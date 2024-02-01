7fafecad5217033b5e901285aac1455eacea6090
njlyon0 <nicholasjlyon@gmail.com>
2024-02-01 11:53:08 -0800

    Updated to extract permafrost (rather than elevation) and use relevant object names

--- extract-permafrost.R
+++ extract-permafrost.R
@@ -9,7 +9,7 @@
 ## Extract the following data: PERMAFROST
 
 ## ------------------------------------------------------- ##
-# Housekeeping ----
+                      # Housekeeping ----
 ## ------------------------------------------------------- ##
 
 # Read needed libraries
@@ -54,19 +54,19 @@ dplyr::glimpse(sheds)
 rm(list = setdiff(ls(), c('path', 'sites', 'sheds')))
 
 ## ------------------------------------------------------- ##
-# Elevation - Extract ----
+                # Permafrost - Extract ----
 ## ------------------------------------------------------- ##
 
 # Read in dataset
-elev_raw <- terra::rast(x = file.path(path, "raw-driver-data", "raw-elevation", 
-                                      "wc2.1_30s_elev.tif"))
+pf_raw <- terra::rast(x = file.path(path, "raw-driver-data", "raw-permafrost", 
+                                    "perprob.tif"))
 
 # Exploratory plot for overlap
-plot(elev_raw, axes = T, reset = F)
+plot(pf_raw, axes = T, reset = F)
 plot(sheds, axes = T, add = T)
 
 # Strip out land cover for our polygons
-elev_out <- exactextractr::exact_extract(x = elev_raw, y = sheds,
+pf_out <- exactextractr::exact_extract(x = pf_raw, y = sheds,
                                          include_cols = c("LTER", "Shapefile_Name")) %>%
   # Above returns a list so switch it to a dataframe
   purrr::map_dfr(dplyr::select, dplyr::everything()) %>%
@@ -74,117 +74,46 @@ elev_out <- exactextractr::exact_extract(x = elev_raw, y = sheds,
   dplyr::filter(!is.na(value))
 
 # Check that dataframe
-dplyr::glimpse(elev_out)
+dplyr::glimpse(pf_out)
 
 ## ------------------------------------------------------- ##
-# Elevation - Summarize ----
+                # Permafrost - Summarize ----
 ## ------------------------------------------------------- ##
 
 # Wrangle extracted data
-elev_actual <- elev_out %>%
+pf_actual <- pf_out %>%
   # Summarize elevation within river ID
   dplyr::group_by(LTER, Shapefile_Name) %>%
-  dplyr::summarize(elevation_median_m = stats::median(value, na.rm = T),
-                   elevation_mean_m = mean(value, na.rm = T),
-                   elevation_min_m = min(value, na.rm = T),
-                   elevation_max_m = max(value, na.rm = T)) %>%
+  dplyr::summarize(permafrost_median_m = stats::median(value, na.rm = T),
+                   permafrost_mean_m = mean(value, na.rm = T),
+                   permafrost_min_m = min(value, na.rm = T),
+                   permafrost_max_m = max(value, na.rm = T)) %>%
   dplyr::ungroup()
 
 # Glimpse this
-dplyr::glimpse(elev_actual)
+dplyr::glimpse(pf_actual)
 
 ## ------------------------------------------------------- ##
-# Basin Slope - Extract & Summarize ----
-## ------------------------------------------------------- ##
-# Create an empty list to store results
-slope_list <- list()
-
-# For each watershed shapefile...
-for (i in 1:nrow(sheds)){
-  
-  # Starting message
-  message("Extracting slope for watershed ", i, " (", (nrow(sheds) - i), " remaining)")
-  
-  # Crop and mask the elevation raster to each shapefile
-  cropped_raster <- terra::crop(x = elev_raw, y = terra::vect(sheds[i,]), mask = TRUE)
-  
-  # Calculate the slopes
-  slope_raster <- terra::terrain(cropped_raster, v = "slope", unit = "degrees")
-  
-  # Extract the slopes into a dataframe
-  slope_dataframe <- terra::as.data.frame(slope_raster)
-  
-  # Create a dummy dataframe if the extracted dataframe has 0 rows
-  if (nrow(slope_dataframe) == 0){
-    LTER_Shapefile_slope_dataframe <- data.frame(LTER = sheds[i,]$LTER,
-                                                 Shapefile_Name = sheds[i,]$Shapefile_Name,
-                                                 basin_slope_median_degree = NA,
-                                                 basin_slope_mean_degree = NA,
-                                                 basin_slope_min_degree = NA,
-                                                 basin_slope_max_degree = NA)
-    
-    # Save the dataframe into our list
-    slope_list[[i]] <- LTER_Shapefile_slope_dataframe
-    
-    # If the dataframe is NOT empty...
-  } else {
-    # Calculate slope statistics
-    LTER_Shapefile_slope_dataframe <- slope_dataframe %>%
-      # Keep LTER/shapefile name for summarizing and joining later
-      dplyr::mutate(LTER = sheds[i,]$LTER,
-                    Shapefile_Name = sheds[i,]$Shapefile_Name) %>%
-      # Calculate slope statistics within those groups
-      dplyr::group_by(LTER, Shapefile_Name) %>%
-      dplyr::summarize(basin_slope_median_degree = stats::median(slope, na.rm = T),
-                       basin_slope_mean_degree = spatialEco::mean_angle(slope, angle = "degree"),
-                       basin_slope_min_degree = min(slope, na.rm = T),
-                       basin_slope_max_degree = max(slope, na.rm = T))
-    
-    # Save the dataframe into our list
-    slope_list[[i]] <- LTER_Shapefile_slope_dataframe }
-  
-} # Close loop
-
-# Unlist into one big dataframe
-slope_actual <- slope_list %>% purrr::map_dfr(.f = select, everything())
-
-# Glimpse this
-dplyr::glimpse(slope_actual)
-
-# Here is an alternative way to extract the slope using the `starsExtra::slope()` function, 
-# but it requires rasters with a projected CRS, not a geographic CRS.
-# So I would need to change the CRS first if I wanted to use `starsExtra::slope()`.
-# I opted to use the `terra::terrain()` function instead.
-
-# cropped_raster <- terra::crop(x = elev_raw, y = sheds[1,])
-# reproj_raster <- terra::project(cropped_raster, "EPSG:3857") # Web Mercator projection
-# converted_raster <- stars::st_as_stars(reproj_raster)
-# slope_raster <- starsExtra::slope(converted_raster)
-# slope_dataframe <- as.data.frame(slope_raster)
-
-## ------------------------------------------------------- ##
-#           Elevation & Basin Slope - Export ----
+                # Permafrost - Export ----
 ## ------------------------------------------------------- ##
 # Let's get ready to export
-elev_export <- sites %>%
+pf_export <- sites %>%
   # Join the rock data
-  dplyr::left_join(y = elev_actual, by = c("LTER", "Shapefile_Name")) %>%
-  # Join the slope data
-  dplyr::left_join(y = slope_actual, by = c("LTER", "Shapefile_Name"))
+  dplyr::left_join(y = pf_actual, by = c("LTER", "Shapefile_Name"))
 
 # Check it out
-dplyr::glimpse(elev_export)
+dplyr::glimpse(pf_export)
 
 # Create folder to export to
 dir.create(path = file.path(path, "extracted-data"), showWarnings = F)
 
 # Export the summarized elevation and slope data
-write.csv(x = elev_export, na = '', row.names = F,
-          file = file.path(path, "extracted-data", "si-extract_elevation.csv"))
+write.csv(x = pf_export, na = '', row.names = F,
+          file = file.path(path, "extracted-data", "si-extract_permafrost.csv"))
 
 # Upload to GoogleDrive
 googledrive::drive_upload(media = file.path(path, "extracted-data", 
-                                            "si-extract_elevation.csv"),
+                                            "si-extract_permafrost.csv"),
                           overwrite = T,
                           path = googledrive::as_id("https://drive.google.com/drive/u/0/folders/1Z-qlt9okoZ4eE-VVsbHiVVSu7V5nEkqK"))
 
